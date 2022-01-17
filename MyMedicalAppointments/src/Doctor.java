de95a95ec06caec85bb389dbb586c70c025bcef3
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-17 16:25:03 -0300

    Constructor overflow

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,15 +1,17 @@
 public class Doctor {
-    static int id = 0;//Autoincrement
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Building Doctor object");
-        id++;
     }
 
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("The name of the assigned Doctor is: " + name);
+        this.name = name;
+        this.speciality = speciality;
+        id++;
     }
 
     //Behaviors
