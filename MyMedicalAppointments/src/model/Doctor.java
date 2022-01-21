aba53ef3e7a330bbbf68415f8fc5032b747803bc
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-21 12:33:21 -0300

    Doctor Menu Modularization

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -24,7 +26,7 @@ public class Doctor extends User{
     //Behaviors
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void  addAvailableAppointment(Date date, String time){
+    public void  addAvailableAppointment(String date, String time){
         availableAppointments.add(new Doctor.AvailableAppointment(date, time));
     }
 
@@ -47,6 +49,7 @@ public class Doctor extends User{
         private  int id;
         private Date date;
         private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy");
         /*Since the appointments only exist if a doctor exists, and it only exists for doctor to use
         * we use an internal class*/
 
@@ -55,8 +58,12 @@ public class Doctor extends User{
 
         /*Note that static internal classes, doesn't need to be instanced*/
 
-        public AvailableAppointment(Date date, String time){
-            this.date = date;
+        public AvailableAppointment(String date, String time){
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -71,7 +78,9 @@ public class Doctor extends User{
         public Date getDate() {
             return date;
         }
-
+        public String getDate(String DATE) {
+            return format.format(date);
+        }
         public void setDate(Date date) {
             this.date = date;
         }
---
+++ MyMedicalAppointments/src/ui/UIDoctorMenu.java
@@ -0,0 +1,88 @@
+package ui;
+
+import model.Doctor;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UIDoctorMenu {
+    public static ArrayList<Doctor> doctorsAvailableAppointments;
+
+    public static void showDoctorMenu(){
+        int response;
+        do{
+            System.out.println("\n\n");
+            System.out.println("Doctor");
+            System.out.println("Welcome: " + UIMenu.doctorLogged.getName());
+            System.out.println("1. Add Available Appointment");
+            System.out.println("2. My Scheduled Appointment");
+            System.out.println("0. Logout");
+
+            Scanner sc = new Scanner(System.in);
+            response = sc.nextByte();
+
+            switch (response){
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    UIMenu.showMenu();
+                    break;
+            }
+        }while (response != 0);
+    }
+
+    private static void showAvailableAppointmentMenu(){
+        int response;
+        do{
+            System.out.println("\n");
+            System.out.println(":: Add Available Appointment");
+            System.out.println(":: Select a Month");
+            for (int i = 0; i < 3; i++) {
+                int j = i +1;
+                System.out.println(j + "." + UIMenu.MONTHS[i]);
+            }
+            System.out.println("0. Return");
+            Scanner sc = new Scanner(System.in);
+            response = sc.nextByte();
+
+            if(response > 0 && response <4){
+                //1,2,3
+                int monthSelected = response;//Change name to be more descriptive
+                System.out.println(monthSelected + " . " + UIMenu.MONTHS[monthSelected-1]);
+                System.out.println("Insert the date available: [dd/mm/yyyy]");
+                String date = sc.nextLine();
+
+                System.out.println("Your date is: " + date + "\n1. Correct \n2. Change date");
+                int responseDate = sc.nextByte();
+                if (responseDate == 2) continue;//Keep on cycle, but jump over next lines
+
+                int responseTime;
+                String time;
+                do {
+                    System.out.println("Insert the time available for date: " + date +
+                            " [hh:mm]");
+                    time = sc.nextLine();
+                    System.out.println("Your time is: " + time + "\n 1. Correct \n 2.Change time");
+                    responseTime = sc.nextByte();
+                }while (responseTime == 2);
+                //If responseTime is 2 (Change time) it keeps on cycle for time selection, but keep date unchanged
+
+                UIMenu.doctorLogged.addAvailableAppointment(date,time);
+                checkDoctorAvailableAppointments(UIMenu.doctorLogged);
+            }else if (response == 0){
+                showDoctorMenu();
+            }
+        }while (response != 0);
+    }
+
+    private static void checkDoctorAvailableAppointments(Doctor doctor){
+        if (doctor.getAvailableAppointments().size() > 0
+            && !doctorsAvailableAppointments.contains(doctor)) {
+            //it should be added only one time
+            doctorsAvailableAppointments.add(doctor);
+        }
+    }
+}
